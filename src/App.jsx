5d6b701c133d7943a4b83494e3ce5aeb4dbc0765
Agustin <agustin.97.ceballos@gmail.com>
2025-01-05 20:32:51 -0300

    add button theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import fotoperfil from "./assets/images/fotoPerfil.jpg"
 import "./App.css"
+import { useState } from "react"
 
 
 function App() {
 
+  const [theme, setTheme] = useState("light");
+
+  const cambiartema = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
 
   return (
     <>
-    <div className="container">
+    <div className={`container ${theme}`}>
+      <button className="theme" onClick={cambiartema}>theme</button>
       <img src={fotoperfil} alt="fotoPerfil" className="fotoperfil"/>
-      <h2 className="h2">Agustín Ceballos</h2>
+      <h2 className={`h2 ${theme}`}>Agustín Ceballos</h2>
       <button className="button"><a className="a" href="https://www.linkedin.com/in/agustin-ceballos/" >Linkedin</a></button>
-      <button className="button"><a className="a" href="https://wa.me/543543316777"  >Whattapp</a></button>
+      <button className="button"><a className="a" href="https://wa.me/543543316777"  >Whatsapp</a></button>
       <button className="button"><a className="a" href="https://github.com/Agustin197" >Github</a></button>
     </div>
     </>
